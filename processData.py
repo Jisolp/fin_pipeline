18b52456baf827d8563ff649c3c9292f438ed1b8
Jisol Park <jisolp7@gmail.com>
2025-02-07 09:05:12 -0800

    process data.py

--- processData.py
+++ processData.py
@@ -3,12 +3,67 @@ import pandas as pd
 import logging
 
 logging.basicConfig(
-    level = logging.INFO,
-    format = '%{asctime}s - %{levelname}s - %{message}s',
-    handler = [logging.FileHandler('cleanData.log'),logging.StreamHandler()]
+    level= logging.INFO,
+    format = "%(asctime)s- %(levelname)s - %(message)s",
+    handlers = [logging.FileHandler('cleanData.log'), logging.StreamHandler()] 
 )
 def processData(rawData, cleanedData):
-    logging.info(f'loading {rawData}')
-    df = pd.read_csv(rawData)
+    try: 
+        logging.info(f'loading {rawData}...')
+        df = pd.read_csv(rawData)
 
-    #missing values
+        #cleaning data - checking for missing columns (alert only)
+        cols = ['1. open','2. high','3. low','4. close','5. volume']
+        logging.info("checking for misisng colums...")
+        for col in cols:
+            if col not in df.columns:
+                logging.error(f"column {col} not found in the data")
+                return 
+            
+        #cleaning data - checking for missing values and dropping the rows 
+        logging.info("checking for missing values...")
+        missingVal = df.isnull().sum()
+        logging.info(f"missing values:\n {missingVal}")
+        cleanDf = df.dropna()
+
+        #standardizing format - dates
+        if '1. date' in cleanDf.columns:
+            logging.info("Standardizing date format...")
+            cleanDf.index = pd.to_datetime(cleanDf.index,error = 'coerce')
+            cleanDf = cleanDf.dropna(subset=['date'])
+
+        #standardizing format - converting number values to numeric 
+        cols = ['1. open','2. high','3. low','4. close','5. volume']
+        for col in cols:
+            if col in cleanDf.columns:
+                logging.info(f"standardizing {col} numeric format")
+                cleanDf[col] = pd.to_numeric(cleanDf[col], errors = 'coerce')
+                cleanDf = cleanDf.dropna(subset=[col])
+        
+        #validate data - check for neg price cols 
+        logging.info(f"Checking for negative prices...")
+        cols = ['1. open','2. high','3. low','4. close','5. volume']
+        for col in cols:
+            if col in cleanDf.columns:
+                cleanDf = cleanDf[cleanDf[col] >= 0]
+        
+        #validate data - volume outlier 
+        logging.info("Checking for outliers in volume...")
+        if '5. volume' in cleanDf.columns:
+            vol99th = cleanDf['5. volume'].quantile(.99)
+            cleanDf = cleanDf[cleanDf['5. volume'] <= vol99th]
+
+        #save data 
+        os.makedirs(os.path.join('data','processed'), exist_ok=True)
+        df.to_csv(os.path.join('data','processed','sp500_processed.csv'))
+        logging.info('data processed and saved')
+    except Exception as e:
+        logging.error(f"Error occured: {e}")
+    
+if __name__ == "__main__":
+    # File paths
+    raw_data_path = "data/raw/sp500_raw.csv"  # Path to the raw data CSV
+    cleaned_data_path = "data/processed/sp500_processed.csv"  # Where to save the cleaned data
+
+    # Run the data processing function
+    processData(raw_data_path, cleaned_data_path)
--- uploadToS3.py
+++ uploadToS3.py
@@ -7,13 +7,16 @@ from dotenv import load_dotenv
 
 logging.basicConfig(
     level = logging.INFO,
-    format = '%{asctime}s - %{levelname}s - %{message}s',
+    format = '%(asctime)s - %(levelname)s - %(message)s',
     handler = [logging.FileHandler('app.log'),logging.StreamHandler()]
 )
 def uploadToS3(fileName, bucketName, objectName = None):
     s3Client = boto3.client('s3')
+    timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+
     if objectName is None:
-        objectName = fileName
+        dataType = os.path.basename(os.path.dirname(fileName))
+        objectName = f"{dataType}/{timestamp}"
     try:
         s3Client.upload_file(fileName,bucketName,objectName)
         logging.info(f"uploaded {fileName} to {bucketName}/{objectName}")
@@ -21,14 +24,15 @@ def uploadToS3(fileName, bucketName, objectName = None):
         logging.info(f"Error uploading: {e}")
 
 
-# if __name__ == '__main__':
-#     API_KEY = os.getenv('API_KEY')
-#     if not API_KEY:
-#         logging.error("API key not found")
-#         exit()
-#     fetchData(API_KEY)
-
-#     bucketName = 'fin-market-data'
-#     rawData = os.path.join('data', 'raw', 'sp500_raw.csv')
-
-#     uploadToS3(rawData, bucketName,objectName='raw/sp500_raw.csv')
+if __name__ == '__main__':
+    API_KEY = os.getenv('API_KEY')
+    if not API_KEY:
+        logging.error("API key not found")
+        exit()
+    fetchData(API_KEY)
+    bucketName = 'fin-market-data'
+    rawData = os.path.join('data', 'raw', 'sp500_raw.csv')
+    processData = os.path.join('data','processed','sp500_processed.csv')
+    
+    uploadToS3(rawData, bucketName)
+    uploadToS3(processData, bucketName)
