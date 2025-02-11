abb8605fd8e5c2ca147dcd8d365e7562e8c746fa
Jisol Park <jisolp7@gmail.com>
2025-02-10 22:41:02 -0800

    upload to rds

--- src/processData.py
+++ src/processData.py
@@ -10,21 +10,21 @@ logging.basicConfig(
 def processData(rawData, cleanedData):
     try: 
         logging.info(f'loading {rawData}...')
-        df = pd.read_csv(rawData)
-        df.columns = ["date", "1. open", "2. high", "3. low", "4. close", "5. volume"]
+        df = pd.read_csv(rawData, index_col = None)
+        df.columns = ["date", "open", "high", "low", "close", "volume"]
 
         #cleaning data - checking for missing columns (alert only)
-        cols = ['1. open','2. high','3. low','4. close','5. volume']
-        logging.info("checking for misisng colums...")
+        cols = ["open", "high", "low", "close", "volume"]
+        logging.info("Checking for misisng colums...")
         for col in cols:
             if col not in df.columns:
-                logging.error(f"column {col} not found in the data")
+                logging.error(f"Column {col} not found in the data")
                 return 
             
         #cleaning data - checking for missing values and dropping the rows 
-        logging.info("checking for missing values...")
+        logging.info("Checking for missing values...")
         missingVal = df.isnull().sum()
-        logging.info(f"missing values:\n {missingVal}")
+        logging.info(f"Missing values:\n {missingVal}")
         cleanDf = df.dropna()
 
         #standardizing format - dates
@@ -34,37 +34,37 @@ def processData(rawData, cleanedData):
             cleanDf['date'] = cleanDf.dropna(subset=['date'])
 
         #standardizing format - converting number values to numeric 
-        cols = ['1. open','2. high','3. low','4. close','5. volume']
+        cols = ["open", "high", "low", "close", "volume"]
         for col in cols:
             if col in cleanDf.columns:
-                logging.info(f"standardizing {col} numeric format")
+                logging.info(f"Standardizing {col} numeric format")
                 cleanDf[col] = pd.to_numeric(cleanDf[col], errors = 'coerce')
                 cleanDf = cleanDf.dropna(subset=[col])
         
         #validate data - check for neg price cols 
         logging.info(f"Checking for negative prices...")
-        cols = ['1. open','2. high','3. low','4. close','5. volume']
+        cols = ["open", "high", "low", "close", "volume"]
         for col in cols:
             if col in cleanDf.columns:
                 cleanDf = cleanDf[cleanDf[col] >= 0]
         
         #validate data - volume outlier 
         logging.info("Checking for outliers in volume...")
-        if '5. volume' in cleanDf.columns:
-            vol99th = cleanDf['5. volume'].quantile(.99)
-            cleanDf = cleanDf[cleanDf['5. volume'] <= vol99th]
+        if 'volume' in cleanDf.columns:
+            vol99th = cleanDf['volume'].quantile(.99)
+            cleanDf = cleanDf[cleanDf['volume'] <= vol99th]
 
         #save data
         os.makedirs(os.path.join('data','processed'), exist_ok=True)
-        df.to_csv(os.path.join('data','processed','sp500_processed.csv'))
+        df.to_csv(os.path.join('data','processed','sp500_processed.csv'),index=False)
         logging.info('data processed and saved')
     except Exception as e:
         logging.error(f"Error occured: {e}")
     
-# if __name__ == "__main__":
-#     # File paths
-#     raw_data_path = "data/raw/sp500_raw.csv"  # Path to the raw data CSV
-#     cleaned_data_path = "data/processed/sp500_processed.csv"  # Where to save the cleaned data
+if __name__ == "__main__":
+    # File paths
+    raw_data_path = "data/raw/sp500_raw.csv"  # Path to the raw data CSV
+    cleaned_data_path = "data/processed/sp500_processed.csv"  # Where to save the cleaned data
 
-#     # Run the data processing function
-#     processData(raw_data_path, cleaned_data_path)
+    # Run the data processing function
+    processData(raw_data_path, cleaned_data_path)
--- src/uploadToRDS.py
+++ src/uploadToRDS.py
@@ -12,23 +12,95 @@ logging.basicConfig(
 
 load_dotenv()
 
+def createDB(connect, dbName):
+    cursor = connect.cursor()
+    cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s;", (dbName,))
+    if cursor.fetchone():
+        logging.info(f"Database {dbName} already exists")
+    else:
+        cursor.execute(f"CREATE DATABASE {dbName};")
+        connect.commit()
+        logging.info(f"Database {dbName} created")
+
+#creates table if it doesn't exist 
+def createTable(cursor,tableName):
+    makeCreateTable = f"""
+    CREATE TABLE IF NOT EXISTS {tableName} (
+        id SERIAL PRIMARY KEY, 
+        date DATE, 
+        open NUMERIC,
+        high NUMERIC,
+        low NUMERIC,
+        close NUMERIC,
+        volume NUMERIC
+    );
+    """
+    cursor.execute(makeCreateTable)
+    logging.info(f"Table {tableName} exists")
+
+#uploads the processed data to RDS
 def uploadToRDS(processedFile):
     rdsHost = os.getenv("RDS_HOST")
     rdsPort = os.getenv("RDS_PORT")
-    rdsDB = os.getenv("RDS_DB")
     rdsUser = os.getenv("RDS_USER")
     rdsPassword = os.getenv("RDS_PASSWORD")
+    rdsDB = "fin_db"
+
+    connect, cursor = None, None
 
     try: 
         logging.info("Connecting to RDS")
         connect = psycopg2.connect(
             host = rdsHost,
             port = rdsPort,
-            dataBase = rdsDB,
+            dbname = "postgres",
+            user = rdsUser,
+            password = rdsPassword,
+        )
+        connect.set_session(autocommit=True) 
+        cursor = connect.cursor()
+        createDB(connect,rdsDB)
+        logging.info(f"Connected to {rdsDB}")
+        connect.close()
+        cursor.close()
+
+        #reconnect with the db name
+        connect = psycopg2.connect(
+            host = rdsHost,
+            port = rdsPort,
+            dbname = rdsDB,
             user = rdsUser,
             password = rdsPassword,
         )
         cursor = connect.cursor()
-        
+
+        table_name = "financial_table"
+        createTable(cursor,table_name)
+        checkTableExists(cursor, table_name)
+
+        with open(processedFile, mode = 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            header = next(reader)
+
+            insertQuery = f"INSERT INTO {table_name}({', '.join(header)}) VALUES ({', '.join(['%s'] * len(header))})"
+
+            logging.info(f"SQL insert query prepared: {insertQuery}")
+
+            for row in reader: 
+                cursor.execute(insertQuery, row)
+        connect.commit()
+        logging.info(f"Data from {processedFile} loaded to RDS table {table_name}")
+
+
     except Exception as e:
-        logging.error("Unexpected error: {e}")
+        logging.error(f"Unexpected error: {e}")
+
+    finally:
+        if connect:
+            cursor.close()
+            connect.close()
+            logging.info("RDS connection closed")
+
+# if __name__ == '__main__':
+#     processedFile = "data/processed/sp500_processed.csv"
+#     uploadToRDS(processedFile)
